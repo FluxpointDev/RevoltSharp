4201b7b8695fb529045e46ba7dd39d8e68304d2d
Dirk Kok <the@dirkkok.nl>
2023-04-11 13:17:00 +0200

    update testbot

--- TestBot/Program.cs
+++ TestBot/Program.cs
@@ -51,15 +51,15 @@ namespace TestBot
             client.OnReactionRemoved += Client_OnReactionRemoved;
         }
 
-        private void Client_OnReactionAdded(Emoji emoji, ServerChannel channel, ServerMember member, Message message) {
-            if (member.Id != Client.CurrentUser.Id) {
-                Client.Rest.AddMessageReactionAsync(channel.Id, message.Id, emoji.Id);
+        private void Client_OnReactionAdded(Emoji emoji, ServerChannel channel, Downloadable<string, ServerMember> memberDownload, Downloadable<string, Message> messageDownload) {
+            if (memberDownload.Id != Client.CurrentUser.Id) {
+                Client.Rest.AddMessageReactionAsync(channel.Id, messageDownload.Id, emoji.Id).GetAwaiter().GetResult();
             }
         }
 
-        private void Client_OnReactionRemoved(Emoji emoji, ServerChannel channel, ServerMember member, Message message) {
-            if (member.Id != Client.CurrentUser.Id) {
-                Client.Rest.RemoveMessageReactionAsync(channel.Id, message.Id, emoji.Id, Client.CurrentUser.Id);
+        private void Client_OnReactionRemoved(Emoji emoji, ServerChannel channel, Downloadable<string, ServerMember> memberDownload, Downloadable<string, Message> messageDownload) {
+            if (memberDownload.Id != Client.CurrentUser.Id) {
+                Client.Rest.RemoveMessageReactionAsync(channel.Id, messageDownload.Id, emoji.Id, Client.CurrentUser.Id).GetAwaiter().GetResult();
             }
         }
 
