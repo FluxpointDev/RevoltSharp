30332704f55262668c4e67a3f91ff301f7a70a36
Builderb <builderbgamer@gmail.com>
2023-04-13 00:52:29 +0100

    Update lib to v4.0.7

--- RevoltSharp/RevoltClient.cs
+++ RevoltSharp/RevoltClient.cs
@@ -46,7 +46,7 @@ namespace RevoltSharp
         /// </summary>
         public string Token { get; internal set; }
 
-        public string Version { get; } = "4.0.6";
+        public string Version { get; } = "4.0.7";
 
         internal bool UserBot { get; set; }
 
