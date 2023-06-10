657a3d42df571aaa35a0739bccee1e59d104fbde
Builderb <builderbgamer@gmail.com>
2023-06-10 23:26:54 +0100

    Update package

--- RevoltSharp/Client/RevoltClient.cs
+++ RevoltSharp/Client/RevoltClient.cs
@@ -85,7 +85,7 @@ public class RevoltClient : ClientEvents
     /// <summary>
     /// Version of the current RevoltSharp lib installed.
     /// </summary>
-    public string Version { get; } = "6.0.0";
+    public string Version { get; } = "6.0.1";
 
     /// <summary>
     /// The current version of the revolt instance connected to.
