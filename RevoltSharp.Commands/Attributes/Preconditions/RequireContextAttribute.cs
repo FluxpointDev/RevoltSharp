c239921a89bd77027aaa0767eb67eec1d2687a8d
Builderb <builderbgamer@gmail.com>
2023-06-18 00:44:27 +0100

    Add context command attribute

--- RevoltSharp.Commands/Attributes/Preconditions/RequireContextAttribute.cs
+++ RevoltSharp.Commands/Attributes/Preconditions/RequireContextAttribute.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RevoltSharp.Commands;
@@ -9,15 +6,43 @@ namespace RevoltSharp.Commands;
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
 public class RequireContextAttribute : PreconditionAttribute
 {
+
+    public RequireContextAttribute(ContextType contextTypes)
+    {
+        Contexts = contextTypes;
+    }
+
     /// <inheritdoc />
     public override string? ErrorMessage { get; set; }
+
+    /// <summary>
+    /// Gets the context required to execute the command.
+    /// </summary>
+    public ContextType Contexts { get; }
 
     /// <inheritdoc />
     public override Task<PreconditionResult> CheckPermissionsAsync(CommandContext context, CommandInfo command, IServiceProvider services)
     {
-        if (context.Server == null)
-            return Task.FromResult(PreconditionResult.FromError(ErrorMessage ?? "You need to run this command in a Revolt server."));
+        bool isValid = false;
 
-        return Task.FromResult(PreconditionResult.FromSuccess());
+        if ((Contexts & ContextType.Server) != 0)
+            isValid = context.Channel is ServerChannel;
+        if ((Contexts & ContextType.DM) != 0)
+            isValid = isValid || context.Channel is DMChannel;
+        if ((Contexts & ContextType.Group) != 0)
+            isValid = isValid || context.Channel is GroupChannel;
+
+        if (isValid)
+            return Task.FromResult(PreconditionResult.FromSuccess());
+
+        return Task.FromResult(PreconditionResult.FromError($"Invalid channel context for command, require contexts are {Contexts}"));
     }
+}
+
+[Flags]
+public enum ContextType
+{
+    DM = 0x01, 
+    Group = 0x02, 
+    Server = 0x03
 }
