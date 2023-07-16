b340eb0088c1154a66a47838e24e5c5fcc9a9ac3
Builderb <builderbgamer@gmail.com>
2023-07-16 02:20:48 +0100

    Add GetAvatarUrl() for PublicBot

--- RevoltSharp/Core/Bots/PublicBot.cs
+++ RevoltSharp/Core/Bots/PublicBot.cs
@@ -14,6 +14,29 @@ public class PublicBot : CreatedEntity
 
     public string? AvatarId { get; internal set; }
 
+    /// <summary>
+    /// Gets the bot's avatar.
+    /// </summary>
+    /// <param name="which">Which avatar to return.</param>
+    /// <param name="size"></param>
+    /// <returns>URL of the image</returns>
+    public string? GetAvatarUrl(AvatarSources which = AvatarSources.Any, int? size = null)
+    {
+        if (!string.IsNullOrEmpty(AvatarId) && (which | AvatarSources.User) != 0)
+        {
+            Conditions.ImageSizeLength(size, nameof(GetAvatarUrl));
+            return $"{Client.Config.Debug.UploadUrl}avatars/{Id}/{AvatarId}{(size != null ? $"?size={size}" : null)}";
+        }
+
+        if ((which | AvatarSources.Default) != 0)
+        {
+            Conditions.ImageSizeLength(size, nameof(GetAvatarUrl));
+            return $"{Client.Config.ApiUrl}users/{Id}/default_avatar{(size != null ? $"?size={size}" : null)}";
+        }
+
+        return null;
+    }
+
     public string? Description { get; internal set; }
 
     /// <summary> Returns a string that represents the current object.</summary>
