ae85b0a5b5ecd35660ad966ed6d7f66dd2965568
Builderb <builderbgamer@gmail.com>
2023-07-16 01:50:22 +0100

    Update query builder to use string.empty and added ToString() override.

--- RevoltSharp/Extensions/QueryBuilder.cs
+++ RevoltSharp/Extensions/QueryBuilder.cs
@@ -2,58 +2,62 @@
 
 namespace RevoltSharp;
 
-	public class QueryBuilder
+public class QueryBuilder
+{
+	private StringBuilder sb = new StringBuilder();
+
+	public QueryBuilder Add(string key, string value)
+		=> AddIf(true, key, value);
+
+	public QueryBuilder AddIf(bool match, string key, string value)
 	{
-		private StringBuilder sb = new StringBuilder();
-
-
-		public QueryBuilder Add(string key, string value)
-			=> AddIf(true, key, value);
-
-		public QueryBuilder AddIf(bool match, string key, string value)
+		if (match)
 		{
-			if (match)
-			{
-				if (sb.Length == 0)
-					sb.Append($"?{key}={value}");
-				else
-					sb.Append($"&{key}={value}");
-			}
-			return this;
+			if (sb.Length == 0)
+				sb.Append($"?{key}={value}");
+			else
+				sb.Append($"&{key}={value}");
 		}
-
-		public QueryBuilder Add(string key, int value)
-			=> AddIf(true, key, value);
-
-		public QueryBuilder AddIf(bool match, string key, int value)
-		{
-			if (match)
-			{
-				if (sb.Length == 0)
-					sb.Append($"?{key}={value}");
-				else
-					sb.Append($"&{key}={value}");
-			}
-			return this;
-		}
-
-		public QueryBuilder Add(string key, bool value)
-			=> AddIf(true, key, value);
-
-		public QueryBuilder AddIf(bool match, string key, bool value)
-		{
-			if (match)
-			{
-				if (sb.Length == 0)
-					sb.Append($"?{key}={value}");
-				else
-					sb.Append($"&{key}={value}");
-			}
-			return this;
-		}
-
-		public string GetQuery()
-		{
-			return sb.ToString();
-		}
+		return this;
 	}
+
+	public QueryBuilder Add(string key, int value)
+		=> AddIf(true, key, value);
+
+	public QueryBuilder AddIf(bool match, string key, int value)
+	{
+		if (match)
+		{
+			if (sb.Length == 0)
+				sb.Append($"?{key}={value}");
+			else
+				sb.Append($"&{key}={value}");
+		}
+		return this;
+	}
+
+	public QueryBuilder Add(string key, bool value)
+		=> AddIf(true, key, value);
+
+	public QueryBuilder AddIf(bool match, string key, bool value)
+	{
+		if (match)
+		{
+			if (sb.Length == 0)
+				sb.Append($"?{key}={value}");
+			else
+				sb.Append($"&{key}={value}");
+		}
+		return this;
+	}
+
+	public string GetQuery()
+	{
+		if (sb.Length == 0)
+			return string.Empty;
+		return sb.ToString();
+	}
+
+	public override string ToString()
+		=> GetQuery();
+}
