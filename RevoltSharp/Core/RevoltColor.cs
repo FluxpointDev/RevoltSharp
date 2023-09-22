504a043c6b8e12c3f8ff4ac107bfca3845f95518
Yoshiboi18303 <yoshiboi18303.t@gmail.com>
2023-09-22 16:26:19 -0400

    Add predefined colors to RevoltColor class, because DX.

--- RevoltSharp/Core/RevoltColor.cs
+++ RevoltSharp/Core/RevoltColor.cs
@@ -5,6 +5,25 @@ namespace RevoltSharp;
 
 public class RevoltColor
 {
+    #region Constant Colors
+
+    public static RevoltColor Red { get; } = new RevoltColor(255, 0, 0);
+    public static RevoltColor DarkRed { get; } = new RevoltColor(139, 0, 0);
+    public static RevoltColor Orange { get; } = new RevoltColor(255, 165, 0);
+    public static RevoltColor Yellow { get; } = new RevoltColor(255, 255, 0);
+    public static RevoltColor Green { get; } = new RevoltColor(0, 255, 0);
+    public static RevoltColor DarkGreen { get; } = new RevoltColor(0, 100, 0);
+    public static RevoltColor Blue { get; } = new RevoltColor(0, 0, 255);
+    public static RevoltColor DarkBlue { get; } = new RevoltColor(0, 0, 139);
+    public static RevoltColor Purple { get; } = new RevoltColor(128, 0, 128);
+    public static RevoltColor Pink { get; } = new RevoltColor(255, 192, 203);
+    public static RevoltColor Black { get; } = new RevoltColor(0, 0, 0);
+    public static RevoltColor White { get; } = new RevoltColor(255, 255, 255);
+    public static RevoltColor Gray { get; } = new RevoltColor(128, 128, 128);
+    public static RevoltColor NotQuiteBlack { get; } = new RevoltColor(54, 57, 63);
+
+    #endregion
+
     public bool IsEmpty { get; internal set; }
     public int R { get; internal set; } = 0;
     public int G { get; internal set; } = 0;
